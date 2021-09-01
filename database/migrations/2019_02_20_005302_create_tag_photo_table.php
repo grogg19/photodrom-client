253cef8c7f463b0e80a8e734f50a55d9adc39b8f
grogg19 <differ@list.ru>
2021-09-01 04:40:49 +0300

    созданы миграции фотографий и тегов

---
+++ database/migrations/2019_02_20_005302_create_tag_photo_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagPhotoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_photo', function (Blueprint $table) {
+
+            $table->unsignedInteger('tag_id');
+            $table->unsignedInteger('photo_id');
+            $table->primary(['tag_id', 'photo_id']);
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_photo');
+    }
+}
