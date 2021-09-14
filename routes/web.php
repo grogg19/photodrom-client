b968e8fe14fae40b56c9255486de206c9542167e
grogg19 <differ@list.ru>
2021-09-14 17:03:21 +0300

    change permission routes

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PhotosController::class, 'index']);
 
-Route::get('/albums/{year}/{month}/thumbnails/big/{name}', [PhotosController::class, 'showThumb'])->middleware('auth');
-Route::get('/albums/{year}/{month}/thumbnails/small/{name}', [PhotosController::class, 'showSmall'])->middleware('auth');
-Route::get('/albums/{year}/{month}/original/{name}', [PhotosController::class, 'showOriginal'])->middleware('auth');
+Route::get('/albums/{year}/{month}/thumbnails/big/{name}', [PhotosController::class, 'showThumb']);
+Route::get('/albums/{year}/{month}/thumbnails/small/{name}', [PhotosController::class, 'showSmall']);
+Route::get('/albums/{year}/{month}/original/{name}', [PhotosController::class, 'showOriginal']);
 
 
 
