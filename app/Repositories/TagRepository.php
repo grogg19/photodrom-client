b0e149b4b81579e876169869c4bde6bbac25db1e
grogg19 <differ@list.ru>
2021-11-01 03:56:58 +0300

    tagsCloud returns limit items & order by quantity tagged photos

--- app/Repositories/TagRepository.php
+++ app/Repositories/TagRepository.php
@@ -7,9 +7,16 @@ use App\Repositories\Interfaces\TagRepositoryInterface;
 
 class TagRepository implements TagRepositoryInterface
 {
+    /**
+     * All tags with photos
+     * @return mixed
+     */
     public function tagsCloud()
     {
         return Tag::has('photos')
+            ->withCount('photos')
+            ->orderBy('photos_count')
+            ->take(7)
             ->get();
     }
 }
