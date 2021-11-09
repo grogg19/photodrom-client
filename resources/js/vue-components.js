de3be0f0d61f9b102da4a47f6d8528f2325eb04f
grogg19 <differ@list.ru>
2021-11-09 09:24:04 +0300

    change vue-components.js

--- resources/js/vue-components.js
+++ resources/js/vue-components.js
@@ -2,6 +2,6 @@ Vue.component('tags-input', require('./components/Tags-input').default)
 Vue.component('photo-list', require('./components/Photo-list').default)
 
 const app = new Vue({
-    el: '#app',
+    el: '#app'
 })
 
